52123cb533af68e6b5b65b14d9d61d0f2815f7a5
Shreyas Deshmukh <shreyasdeshmukh1982@gmail.com>
2025-11-19 09:24:57 +0530

    solved user db problem

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    provider: {
+        type: String,
+        default: "credentials",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
